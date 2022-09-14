dfa56d02a23e2a01524af69446a0d53a46aa3e9e
KrzysztofHum <k.humienny1996@protonmail.com>
2022-09-14 10:09:47 +0200

    send real-time chat message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,93 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext.js";
+import { ChatContext } from "../context/ChatContext.js";
 import Img from "../img/img.png";
 import Attach from "../img/attach.png";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase.js";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 function Input() {
+  const [text, setText] = useState("");
+  const [file, setFile] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (file) {
+      const storageRef = ref(storage, uuid());
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                file: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setFile(null);
+  };
   return (
     <div className="input">
-      <input type="text" placeholder="Type something..." />
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <div className="send">
         <img src={Attach} alt="" />
-        <input type="file" style={{ display: "none" }} id="file" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <label htmlFor="file">
           <img src={Img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   );
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,20 +1,37 @@
-import React from "react";
-import { useContext } from "react";
+import userEvent from "@testing-library/user-event";
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import { AuthContext } from "../context/AuthContext.js";
 import { ChatContext } from "../context/ChatContext.js";
 
 function Message({ message }) {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
   return (
-    <div className="message owner">
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
-        <img src="" alt="" />
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoUrl
+          }
+          alt=""
+        />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>Hello</p>
-        <img src="" alt="" />
+        <p>{message.text}</p>
+        {message.file && <img src={message.file} alt="" />}
       </div>
     </div>
   );
